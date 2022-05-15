ee05faf53e098d90a406418f62ff16edc7f50dcb
rasaqsodiq1221 <rasaqsodiq1221@gmail.com>
2022-05-15 23:47:50 +0100

    completed mobile design

---
+++ script.js
@@ -0,0 +1,13 @@
+let icon = document.getElementById('icon');
+
+icon.addEventListener('click', toggleIcon);
+
+function toggleIcon() {
+    document.body.classList.toggle('darkTheme');
+    if (document.body.classList.contains('darkTheme')) {
+        icon.className = "fas fa-sun";
+    }
+    else {
+        icon.className = "fas fa-moon"; 
+    }
+}
