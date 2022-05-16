29132554c019380b90547d1f4740cca8a5b7e7ab
rasaqsodiq1221 <rasaqsodiq1221@gmail.com>
2022-05-16 15:53:14 +0100

    rough idea implemented with javascript

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ function toggleIcon() {
         icon.className = "fas fa-moon"; 
     }
 }
+
+let firstBtn = document.getElementById('firstBtn');
+let secondBtn = document.getElementById('secondBtn');
+let thirdBtn = document.getElementById('thirdBtn');
+let fourthBtn = document.getElementById('fourthBtn');
+
+
+let characters = [];
+
+for (let i=32; i<127; i++) {
+    characters.push( String.fromCharCode(i) );
+}
+
+function generatePassword() { 
+    return randomChar = { 
+        first:`${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}`,
+        second:`${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}`,
+        third:`${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}`,
+        fourth:`${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}${characters[Math.floor(Math.random() * 96)]}`
+        
+
+    }
+
+}
+
+
+
+firstBtn.textContent = generatePassword().first
+secondBtn.textContent = generatePassword().second
+thirdBtn.textContent = generatePassword().third
+fourthBtn.textContent = generatePassword().fourth
